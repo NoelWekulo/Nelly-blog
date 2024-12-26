c5ad649fddf18bb9cb12771ca18cc18b17743c6c
Joseph <indiekaj@gmail.com>
2024-12-26 22:37:25 +0300

    created blog and blog category model

--- HealthApp/models.py
+++ HealthApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class BlogCategory(models.Model):
+    category = models.CharField(max_length=90, unique=True)
+    
+    def __str__(self):
+        return self.category
+    
+    class Meta:
+        verbose_name_plural = "Blog Categories"
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE, related_name='blog_category')
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to='blog_images/')
+    date_posted = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    
+    def save(self, *args, **kwargs):
+       if not self.slug:
+           self.slug = slugify(self.title)
+       super().save(*args, **kwargs) 
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+
